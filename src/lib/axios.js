b7bd7acf68a75c55c67056d9976c152843119d54
zuhalcode <zuhalcode@gmail.com>
2023-02-26 17:00:45 +0700

    change config of axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:3000/api", // replace with your API URL
+  baseURL: process.env.BASE_URL, // replace with your API URL
   timeout: 10000, // set the maximum timeout for requests
   headers: {
     "Content-Type": "application/json",
