eafe5b11f5bfe4bf1e431a2ec0a9e618bfc46c78
zuhalcode <zuhalcode@gmail.com>
2023-03-01 18:46:34 +0700

    test change

--- src/components/UI/atoms/Logo.jsx
+++ src/components/UI/atoms/Logo.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
-const Logo = ({ className = "", color = "bg-none" }) => {
+const Logo = ({ className = "" }) => {
   return (
     <div
-      className={`flex w-fit items-center justify-center gap-2 rounded-sm ${color} p-3 text-center ${className}`}
+      className={`flex w-fit items-center justify-center gap-2 rounded-sm p-3 text-center ${className}`}
     >
       <img src="/img/ruesto-logo.png" className="h-14 w-12" alt="" />
       <p className="mt-2 font-berkshire text-3xl font-bold uppercase tracking-wider text-accent">
